5dcbde3e2fd4ce323b934edcf70f62a491c8325b
Kachin <mammothkachin@gmail.com>
2025-07-15 18:24:59 +0700

    add onboard LED

--- TestSite/TestBoardESP32/src/main.cpp
+++ TestSite/TestBoardESP32/src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 
+#define LED_PIN 2 // Onboard LED for most ESP32 boards
+
 void setup() {
-  Serial.begin(9600); // Start serial communication at 115200 baud
+  Serial.begin(9600); // Start serial communication at 9600 baud
   while (!Serial) {
     ; // Wait for serial port to connect. Needed for native USB
   }
+  pinMode(LED_PIN, OUTPUT); // Set onboard LED as output
 }
 
 void loop() {
   Serial.println("Board is alive!"); // Print message every second
-  delay(1000); // Wait for 1 second
-}
+  digitalWrite(LED_PIN, HIGH); // Turn LED on
+  delay(500); // Wait 0.5 second
+  digitalWrite(LED_PIN, LOW); // Turn LED off
+  delay(500); // Wait
 
+}
