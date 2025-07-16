462db001a215f4ae68ac02f5b8c888d6a7e4b377
Kachin <mammothkachin@gmail.com>
2025-07-16 12:21:57 +0700

    Rearrange Folder

--- TestIO/TestOutputMycobot280/.pio/build/upesy_wroom/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/upesy_wroom/FrameworkArduino/USB.cpp.o: \
- /home/shiro/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /home/shiro/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/shiro/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
--- TestIO/TestOutputMycobot280/.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.o: \
- /home/shiro/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/shiro/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
